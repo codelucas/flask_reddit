a7e48a20150cb36207c92e5a2c0864fb6cbdeec8
codelucas <lucasyangpersonal@gmail.com>
2014-01-05 02:11:02 +0000

    added extra model classes and creating relationships

--- flask_reddit/threads/constants.py
+++ flask_reddit/threads/constants.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
+# For simplicity, these values are shaed among both threads and comments.
 MAX_TITLE = 300
 MAX_BODY = 3000
 MAX_LINK = 250
 MAX_COMMENTS = 1000
 
-# thread status
+MAX_COMMENTS = 500
+MAX_DEPTH = 10
+
+# thread & comment status
 DEAD = 0
 ALIVE = 1
 
--- flask_reddit/threads/models.py
+++ flask_reddit/threads/models.py
@@ -4,6 +4,27 @@
 from flask_reddit import db
 from flask_reddit.threads import constants as THREAD
 
+
+thread_upvotes = db.Table('thread_upvotes',
+    db.Column('user_id', db.Integer, db.ForeignKey('users_user.id')),
+    db.Column('thread_id', db.Integer, db.ForeignKey('threads_thread.id'))
+)
+
+thread_downvotes = db.Table('thread_downvotes',
+    db.Column('user_id', db.Integer, db.ForeignKey('users_user.id')),
+    db.Column('thread_id', db.Integer, db.ForeignKey('threads_thread.id'))
+)
+
+comment_upvotes = db.Table('comment_upvotes',
+    db.Column('user_id', db.Integer, db.ForeignKey('users_user.id')),
+    db.Column('comment_id', db.Integer, db.ForeignKey('threads_comment.id'))
+)
+
+comment_downvotes = db.Table('comment_downvotes',
+    db.Column('user_id', db.Integer, db.ForeignKey('users_user.id')),
+    db.Column('comment_id', db.Integer, db.ForeignKey('threads_comment.id'))
+)
+
 class Thread(db.Model):
     """
     We will mimic reddit, with votable threads. Each thread may have either
@@ -22,7 +43,7 @@ class Thread(db.Model):
 
     # upvotes = db.Column()
     # downvotes = db.Column()
-    # comments = db.Column()
+    comments = db.relationship('Comment', backref='thread', lazy='dynamic')
 
     status = db.Column(db.SmallInteger, default=THREAD.ALIVE)
 
@@ -49,7 +70,71 @@ class Thread(db.Model):
 
     def get_human_age(self):
         """
-        returns a humanized version of the raw age of this thread
-        in seconds, eg: 34 minutes ago versus 2040 seconds ago.
+        returns a humanized version of the raw age of this thread,
+        eg: 34 minutes ago versus 2040 seconds ago.
         """
         pass
+
+    def comment_on(self):
+        """
+        when someone comments on this particular thread
+        """
+        pass
+
+class Comment(db.Model):
+    """
+    This class is here because comments can only be made on threads,
+    so it is contained completly in the threads module.
+
+    Note the parent_id and children values. A comment can be commented
+    on, so a comment has a one to many relationship with itself.
+
+    Backrefs:
+        A comment can refer to its parent thread with 'thread'
+        A comment can refer to its parent comment (if exists) with 'parent'
+    """
+    __tablename__ = 'threads_comment'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(THREAD.MAX_BODY), default=None)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users_user.id'))
+    thread_id = db.Column(db.Integer, db.ForeignKey('threads_thread.id'))
+
+    parent_id = db.Column(db.Integer, db.ForeignKey('threads_comment.id'))
+    children = db.relationship('Comment', backref='parent', lazy='dynamic')
+    depth = db.Column(db.Integer, default=1) # start at depth 1
+
+    created_on = db.Column(db.DateTime, default=db.func.now())
+    updated_on = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+    # upvotes = db.Column(db.Integer)
+    # downvotes = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Comment %r>' % (self.body[:25])
+
+    def __init__(self, thread_id, comment_id, user_id, body):
+        self.thread_id = thread_id
+        self.user_id = user_id
+        self.body = body
+
+    def get_age(self):
+        """
+        returns the raw age of this comment in seconds
+        """
+        pass
+
+    def get_human_age(self):
+        """
+        returns a humanized version of the raw age of this comment,
+        eg: 34 minutes ago versus 2040 seconds ago.
+        """
+        pass
+
+    def comment_on(self):
+        """
+        when someone comments on this particular comment
+        """
+        pass
+
+
